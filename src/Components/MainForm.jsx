05dfca6b91df3c65f15c3ab39bc328a9aa21a481
Gayathri H G <102051237+GayathriHG@users.noreply.github.com>
2024-02-26 15:22:21 +0530

    feat: added style and alignment to form (#8)
    * added form
    
    * changes the letter case
    
    * added style to form

--- src/Components/MainForm.jsx
+++ src/Components/MainForm.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import FormInput from "./FormInput";
 import SubmitButton from "./SubmitButton";
 import FormSelect from "./FormSelect";
+
 const MainForm = () => {
   const {
     register,
@@ -16,15 +17,52 @@ const MainForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(formSubmit)}>
-      <div>Main Form</div>
+    <form className="bg-[#0f0f0f] text-white max-w-3xl mx-auto mt-10 p-10 rounded" onSubmit={handleSubmit(formSubmit)}>
+      <div >Main Form</div>
       <FormInput
+      label={"Enter your Name"}
+
         name={"enterName"}
-        label={"Enter Your Name Pls?"}
         placeholder={"Enter Your Name"}
         register={register("enterName")}
+        
       />
-      <FormSelect name={"select"} register={register("select")} />
+      <FormInput
+            label={"Enter  your video title"}
+
+        name={"enterTitle"}
+        placeholder={"Enter Your video title"}
+        register={register("enterTitle")}
+      />
+      <FormInput
+        name={"enterDuration"}
+        label={"Enter the Duration"}
+        placeholder={"Enter the Duration"}
+        register={register("enterDuration")}
+        type="number"
+      />
+      <FormInput
+        name={"enterThumbnail"}
+        label={"Enter the Thumbnail URL"}
+        placeholder={"Enter your Thumbnail URL"}
+        register={register("enterThumbnail")}
+        
+      />
+      <FormInput
+        name={"enterProfile"}
+        label={"Enter the Profile URL"}
+        placeholder={"Enter your Thumbnail URL"}
+        register={register("enterProfile")}
+        
+      />
+      
+
+      <FormSelect name={"select"}
+      label={"Select a Category"} 
+       register={register("select")}
+      />
+      
+      
       <SubmitButton label={"Submit Now"} />
     </form>
   );
