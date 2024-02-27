fe54722cc80d0996342d16e480ca861c37a1d265
Shajid <shajid1biz@gmail.com>
2024-02-27 15:38:20 +0530

    fix: button sizes, and route to add-vids

--- src/Components/BackgroundBoxes.jsx
+++ src/Components/BackgroundBoxes.jsx
@@ -1,8 +1,9 @@
 import { Boxes } from "../Components/ui/background-boxes";
 import { cn } from "../utils/cn";
 import { TypewriterEffect } from "../Components/ui/typewriter-effect";
+import { Link } from "react-router-dom";
 
-export function BackgroundBoxesDemo({ children }) {
+export function BackgroundBoxesDemo() {
   const words = [
     {
       text: "Share",
@@ -26,17 +27,17 @@ export function BackgroundBoxesDemo({ children }) {
   ];
 
   return (
-    <div className="h-[600px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <div className="h-[640px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
       <TypewriterEffect className={cn("md:text-4xl text-xl text-white relative z-20")} words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10 z-20">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+        <button className="w-40 h-12 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           Join now
         </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
+        <button className="w-40 h-12 rounded-xl bg-white text-black border border-black  text-sm">
+          <Link to={"/add-videos"}>Add New Videos</Link>
         </button>
       </div>
       {/* <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
